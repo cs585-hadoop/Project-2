025306fa24c568f035782e29cd8dfa2347febcb8
hadoop <hadoop@localhost>
2017-02-21 19:35:29 -0500

    all seems work for problem1

--- src/project2/Problem1.java
+++ src/project2/Problem1.java
@@ -19,9 +19,32 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Problem1 {
+	static String param = null;
 
 	public static class Problem1Mapper extends Mapper<Object, Text, IntWritable, Text> {
 		private Text word = new Text();
+		static float x1, x2, y1, y2;
+
+		protected void setup(Context context) {
+			if (param != null) {
+				String[] op = param.split(",");
+				x1 = Float.parseFloat(op[0]);
+				y1 = Float.parseFloat(op[1]);
+				x2 = Float.parseFloat(op[2]);
+				y2 = Float.parseFloat(op[3]);
+				float temp;
+				if (x1 > x2) {
+					temp = x2;
+					x2 = x1;
+					x1 = temp;
+				}
+				if (y1 > y2) {
+					temp = y2;
+					y2 = y1;
+					y1 = temp;
+				}
+			}
+		}
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString());
@@ -30,11 +53,25 @@ public class Problem1 {
 				word.set(itr.nextToken());
 				String[] s = word.toString().split(",");
 				if (s.length == 2) {
-					context.write(new IntWritable((int) Float.parseFloat(s[0])), word);
+					if (param != null) {
+						Float px = Float.parseFloat(s[0]);
+						Float py = Float.parseFloat(s[1]);
+						if (px < x1 || px > x2 || py < y1 || py > y2)
+							continue;
+					}
+					context.write(new IntWritable(Math.round(Float.parseFloat(s[0]))), word);
 				}
 				if (s.length == 5) {
-					start = (int) Float.parseFloat(s[1]);
-					end = (int) Math.ceil(Float.parseFloat(s[1]) + Float.parseFloat(s[4]));
+					if (param != null) {
+						float rx = Float.parseFloat(s[1]);
+						float ry = Float.parseFloat(s[2]);
+						float rx2 = rx + Float.parseFloat(s[3]);
+						float ry2 = ry - Float.parseFloat(s[4]);
+						if (rx > x2 || rx2 < x1 || ry < y1 || ry2 > y2)
+							continue;
+					}
+					start = Math.round(Float.parseFloat(s[1]));
+					end = Math.round(Float.parseFloat(s[1]) + Float.parseFloat(s[4]));
 					for (int i = start; i <= end; i++) {
 						context.write(new IntWritable(i), word);
 					}
@@ -113,41 +150,13 @@ public class Problem1 {
 					}
 					if (px < rx || px > rx2)
 						continue;
-					String temp = result.get(r[0]);
-					if (temp != null)
-						result.put(r[0], temp + ",(" + point + ")");
+					if (result.get(r[0]) != null)
+						result.put(r[0], result.get(r[0]) + ",(" + point + ")");
 					else
-						result.put(r[0], "(" + point + ")");		
+						result.put(r[0], "(" + point + ")");
 				}
 			}
-/*
-			for (String point : points) {
-				String[] p = point.split(",");
-				float px = Float.parseFloat(p[0]);
-				float py = Float.parseFloat(p[1]);
 
-				for (String rect : rects) {
-					String[] r = rect.split(",");
-					float ry = Float.parseFloat(r[2]);
-					if (py > ry)
-						continue;
-					float ry2 = ry - Float.parseFloat(r[3]);
-					if (py < ry2)
-						continue;
-					float rx = Float.parseFloat(r[1]);
-					if (px < rx)
-						continue;
-					float rx2 = rx + Float.parseFloat(r[4]);
-					if (px <= rx2) {
-						String temp = result.get(r[0]);
-						if (temp != null)
-							result.put(r[0], temp + ",(" + point + ")");
-						else
-							result.put(r[0], "(" + point + ")");
-					}
-				}
-			}
-*/
 		}
 
 		protected void cleanup(Context context) throws IOException, InterruptedException {
@@ -160,8 +169,8 @@ public class Problem1 {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		if (args.length != 2) {
-			System.err.println("Usage: Problem-1 <HDFS input file> <HDFS output file>");
+		if (args.length != 2 && args.length != 3) {
+			System.err.println("Usage: Problem-1 <HDFS input file> <HDFS output file> <optional-W(x1,y1,x2,y2)>");
 			System.exit(2);
 		}
 		Job job = new Job(conf, "Problem-1");
@@ -171,6 +180,8 @@ public class Problem1 {
 		job.setNumReduceTasks(2);
 		job.setOutputKeyClass(IntWritable.class);
 		job.setOutputValueClass(Text.class);
+		if (args.length == 3)
+			param = args[2];
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
